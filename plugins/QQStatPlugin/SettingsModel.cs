efe655947659617f4ea06236ab27f2814f0c5962
yiyun <yiyungent@gmail.com>
2022-04-15 15:21:45 +0800

    feat(plugins/qqstatplugin/): add

---
+++ plugins/QQStatPlugin/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using PluginCore.Models;
+
+namespace QQStatPlugin
+{
+    public class SettingsModel : PluginSettingsModel
+    {
+        public string Hello { get; set; }
+
+        public string AdminQQ { get; set; }
+    }
+}
