b71494d2a2768ad392b410fe2ee24d8e19701777
yiyun <yiyungent@gmail.com>
2022-04-15 12:50:48 +0800

    feat(test/consoleapp/program.cs): botKeyStore.json

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -2,12 +2,10 @@
 using Konata.Core.Common;
 using Konata.Core.Interfaces;
 using Konata.Core.Interfaces.Api;
+using System.Text.Json;
 using static Konata.Core.Events.Model.CaptchaEvent;
 
 Console.WriteLine("Hello, World!");
-
-
-
 
 
 // Create a bot instance
@@ -16,6 +14,12 @@ string uin = Console.ReadLine().Trim();
 Console.Write("Password: ");
 string password = Console.ReadLine();
 var botKeystore = new BotKeyStore(uin: uin, password: password);
+
+string logsDir = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+if (!Directory.Exists(logsDir))
+{
+    Directory.CreateDirectory(logsDir);
+}
 var bot = BotFather.Create(BotConfig.Default(), BotDevice.Default(), botKeystore);
 {
     // Handle the captcha
@@ -35,15 +39,22 @@ var bot = BotFather.Create(BotConfig.Default(), BotDevice.Default(), botKeystore
 
     // Print the log
     bot.OnLog += (_, e)
-        => Console.WriteLine(e.EventMessage);
+        =>
+    {
+        string logFilePath = Path.Combine(logsDir, DateTime.Now.ToString("yyyy-MM-dd HH"));
+        File.AppendAllLines(logFilePath, new string[] {
+            DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff"),
+            e.EventMessage
+        }, System.Text.Encoding.UTF8);
+    };
 
     // Handle group messages
     bot.OnGroupMessage += (_, e)
-        => Console.WriteLine(e.Message);
+        => Console.WriteLine($"群消息: {e.Message.Chain?.FirstOrDefault()?.ToString() ?? ""}");
 
     // Handle friend messages
     bot.OnFriendMessage += (_, e)
-        => Console.WriteLine(e.Message);
+        => Console.WriteLine($"好友消息: {e.Message.Chain?.FirstOrDefault()?.ToString() ?? ""}");
 
     // ... More handlers
 }
@@ -56,5 +67,13 @@ if (!await bot.Login())
 }
 
 Console.WriteLine("We got online!");
+
+Console.WriteLine("下方是你的 BotKeyStore.json, 请将它复制作为 环境变量 BOT_KEYSTORE 的值");
+Console.WriteLine("-------------------------------start-------------------------------------");
+string jsonStr = JsonSerializer.Serialize(bot.KeyStore,
+               new JsonSerializerOptions { WriteIndented = true });
+File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "BotKeyStore.json"), jsonStr, System.Text.Encoding.UTF8);
+Console.WriteLine(jsonStr);
+Console.WriteLine("-------------------------------end-------------------------------------");
 
 
