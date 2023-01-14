23aaf866417437893e7b3496a0f5518de6c4c802
yiyun <yiyungent@gmail.com>
2023-01-14 17:07:07 +0800

    feat(plugins/moliplugin/): 依赖KonataPlugin

--- plugins/MoLiPlugin/Utils/MoLiApiUtil.cs
+++ plugins/MoLiPlugin/Utils/MoLiApiUtil.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using KonataPlugin.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using QQBotHub.Sdk.Utils;
 
 namespace MoLiPlugin.Utils
 {
