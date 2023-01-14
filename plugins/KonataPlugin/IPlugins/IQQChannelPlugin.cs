f2cf56679c00e11d106fd07236bded941cbc1aa1
yiyun <yiyungent@gmail.com>
2023-01-14 14:02:45 +0800

    feat(plugins/konataplugin): init

---
+++ plugins/KonataPlugin/IPlugins/IQQChannelPlugin.cs
@@ -0,0 +1,32 @@
+﻿using PluginCore.IPlugins;
+using QQChannelFramework.Api;
+using QQChannelFramework.Models.MessageModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQChannelPlugin.IPlugins
+{
+    public interface IQQChannelPlugin : IPlugin
+    {
+        void OnConnected(string botAppId);
+
+        void AuthenticationSuccess(string botAppId);
+
+        void OnError(string botAppId, Exception ex);
+
+        /// <summary>
+        /// 注意: message.Content 中包含 <@!1094789961239705377> 其中 1094789961239705377 为@的人
+        /// </summary>
+        /// <param name="botAppId"></param>
+        /// <param name="message"></param>
+        /// <param name="qChannelApi"></param>
+        void ReceivedAtMessage(string botAppId, Message message, QQChannelApi qChannelApi);
+
+        void ReceivedDirectMessage(string botAppId, Message message, QQChannelApi qChannelApi);
+
+        void ReceivedUserMessage(string botAppId, Message message, QQChannelApi qChannelApi);
+    }
+}
---
+++ plugins/KonataPlugin/README.md
@@ -0,0 +1,35 @@
+
+
+
+## 介绍
+
+> 本项目为 QQ频道 基础插件, 可通过依赖本插件开发相关插件
+
+```json
+"UseSandBoxMode": true,
+"UseDemoModel": true
+```
+
+> 当开启 `UseDemoModel` 时, 可在机器人加入的频道内 `@机器人` , 它会自动回复, 此项功能用于测试
+
+
+## 登录
+
+- [点击访问即可尝试登录, 注意查看控制台信息](/Plugins/QQChannelPlugin/Login)
+
+
+
+## 下载数据库
+
+> 所有数据保存到数据库 **QQChannelPlugin.sqlite** 中
+
+- [下载数据库 (QQChannelPlugin.sqlite)](/Plugins/QQChannelPlugin/Download)
+
+
+
+
+## 相关
+
+> 项目地址: [https://github.com/yiyungent/QQBotHub/tree/main/plugins/QQChannelPlugin](https://github.com/yiyungent/QQBotHub/tree/main/plugins/QQChannelPlugin)             
+> 本项目基于 [Chianne1025/QQChannelFramework: MyBot - QQ频道机器人开发框架(C#)](https://github.com/Chianne1025/QQChannelFramework)
+
