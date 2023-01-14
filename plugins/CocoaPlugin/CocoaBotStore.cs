b4b608b722f675307bf203a987948679f9b2605c
yiyun <yiyungent@gmail.com>
2023-01-14 18:36:33 +0800

    feat(plugins/cocoaplugin/): init

---
+++ plugins/CocoaPlugin/CocoaBotStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QQChannelFramework.Api;
+using QQChannelFramework.Expansions.Bot;
+
+namespace QQChannelPlugin
+{
+    public static class QQChannelBotStore
+    {
+        public static List<BotItemModel> Bots { get; set; }
+
+        static QQChannelBotStore()
+        {
+            Bots = new List<BotItemModel>();
+        }
+
+        public sealed class BotItemModel
+        {
+            public OpenApiAccessInfo OpenApiAccessInfo { get; set; }
+
+            public QQChannelApi QQChannelApi { get; set; }
+
+            public ChannelBot ChannelBot { get; set; }
+        }
+    }
+}
