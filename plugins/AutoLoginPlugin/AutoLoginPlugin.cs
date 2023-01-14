fa5e786277f8b875a59f88d09df82f4f16a96987
yiyun <yiyungent@gmail.com>
2023-01-14 17:01:35 +0800

    fix(plugins/autologinplugin/): 依赖KonataPlugin

--- plugins/AutoLoginPlugin/AutoLoginPlugin.cs
+++ plugins/AutoLoginPlugin/AutoLoginPlugin.cs
@@ -6,17 +6,16 @@ using PluginCore.IPlugins;
 using Konata.Core;
 using Konata.Core.Events.Model;
 using Konata.Core.Interfaces.Api;
-using QQBotHub.Sdk.IPlugins;
 using AutoLoginPlugin.Utils;
 using Konata.Core.Message.Model;
 using Konata.Core.Common;
 using System.Text;
-using QQBotHub.Sdk;
 using Konata.Core.Message;
 using System.Collections.Generic;
 using Konata.Core.Interfaces;
 using System.IO;
 using PluginCore;
+using KonataPlugin;
 
 namespace AutoLoginPlugin
 {
@@ -46,9 +45,9 @@ namespace AutoLoginPlugin
             {
                 SettingsModel settingsModel = PluginCore.PluginSettingsModelFactory.Create<SettingsModel>(nameof(AutoLoginPlugin));
                 string filePath = Path.Combine(PluginPathProvider.PluginsRootPath(), nameof(AutoLoginPlugin), "BotKeyStore.json");
-                if (QQBotStore.Bot != null && QQBotStore.Bot.IsOnline() && QQBotStore.Bot.KeyStore != null)
+                if (KonataBotStore.Bot != null && KonataBotStore.Bot.IsOnline() && KonataBotStore.Bot.KeyStore != null)
                 {
-                    string jsonStr = QQBotHub.Sdk.Utils.JsonUtil.Obj2JsonStr(QQBotStore.Bot.KeyStore);
+                    string jsonStr = KonataPlugin.Utils.JsonUtil.Obj2JsonStr(KonataBotStore.Bot.KeyStore);
                     File.WriteAllText(filePath, contents: jsonStr, Encoding.UTF8);
                 }
                 // 取消: 发现这么调用, 会触发验证, 而直接 用 BotKeyStore new 一个, 就不会(好多次这样都没有验证)
@@ -64,8 +63,8 @@ namespace AutoLoginPlugin
                     {
                         #region 重新 new
                         string jsonStr = File.ReadAllText(filePath, Encoding.UTF8);
-                        BotKeyStore botKeyStore = QQBotHub.Sdk.Utils.JsonUtil.JsonStr2Obj<BotKeyStore>(jsonStr);
-                        QQBotStore.Bot = BotFather.Create(BotConfig.Default(), BotDevice.Default(), botKeyStore);
+                        BotKeyStore botKeyStore = KonataPlugin.Utils.JsonUtil.JsonStr2Obj<BotKeyStore>(jsonStr);
+                        KonataBotStore.Bot = BotFather.Create(BotConfig.Default(), BotDevice.Default(), botKeyStore);
                         #endregion
 
                         #region 重新登录
@@ -87,7 +86,7 @@ namespace AutoLoginPlugin
             bool isLoginSuccess = false;
             try
             {
-                isLoginSuccess = await QQBotStore.Bot.Login();
+                isLoginSuccess = await KonataBotStore.Bot.Login();
             }
             catch (Exception ex)
             {
@@ -98,7 +97,7 @@ namespace AutoLoginPlugin
             {
                 if (!string.IsNullOrEmpty(settingsModel.AdminQQ) && uint.TryParse(settingsModel.AdminQQ, out uint adminUin))
                 {
-                    await QQBotStore.Bot.SendFriendMessage(friendUin: adminUin, "自动重新登录成功");
+                    await KonataBotStore.Bot.SendFriendMessage(friendUin: adminUin, "自动重新登录成功");
                 }
             }
             else
