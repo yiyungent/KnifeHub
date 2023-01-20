320046d6b0d51dda0c77a6964b4a2a7e880985c1
yiyun <yiyungent@gmail.com>
2023-01-20 13:56:57 +0800

    docs(telegramplugin/iplugins/itelegrambotplugin.cs): handleUpdateAsync:add 注释
    TgBot接收所有消息需禁用 Group.privacy mode

--- plugins/TelegramPlugin/IPlugins/ITelegramBotPlugin.cs
+++ plugins/TelegramPlugin/IPlugins/ITelegramBotPlugin.cs
@@ -12,6 +12,17 @@ namespace PluginCore.IPlugins
 {
     public interface ITelegramBotPlugin : IPlugin
     {
+        /// <summary>
+        /// 注意: 默认你的 TgBot 在 Group 中只能收到 /command@YourBotName 指令消息, 
+        /// 若需要接收所有消息, 需要关闭 privacy mode, 然后重新邀请 你的 TgBot 到 Group
+        /// /setprivacy — Set which messages your bot will receive when added to a group. With privacy mode disabled, the bot will receive all messages.
+        /// 参考:   
+        /// https://stackoverflow.com/questions/44751015/receive-messages-from-groups-or-channels-using-telegram-bot-api
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="botToken"></param>
         void HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken, string botToken);
 
         void HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken, string botToken);
