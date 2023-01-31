63a03ba2f4fdea8d56893987b99a5be2dfc63ea9
yiyun <yiyungent@gmail.com>
2023-01-31 13:39:59 +0800

    feat(src/knifehub.web/): add Sentry.AspNetCore

--- src/KnifeHub.Web/Program.cs
+++ src/KnifeHub.Web/Program.cs
@@ -5,6 +5,18 @@ using PluginCore.lmplements;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = "https://ed1fa11da1bb47188350fbcfcb6af159@o4504597240741888.ingest.sentry.io/4504597253980160";
+    // When configuring for the first time, to see what the SDK is doing:
+    //o.Debug = true;
+    // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    o.TracesSampleRate = 1.0;
+});
+
+//Sentry.SentrySdk.CaptureMessage("Hello Sentry");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -21,6 +33,12 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddPluginCore();
 
 var app = builder.Build();
+
+// Performance monitoring
+// Enable automatic tracing integration.
+// If running with .NET 5 or below, make sure to put this middleware
+// right after `UseRouting()`.
+app.UseSentryTracing();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
