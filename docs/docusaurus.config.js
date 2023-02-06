7b782c56b9405445fb985ecc5b58a0cf95068c30
yiyun <yiyungent@gmail.com>
2023-02-06 15:33:26 +0800

    docs(docs/docusaurus.config.js): update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,15 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://moeci.com/KnifeHub',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yiyungent', // Usually your GitHub org/user name.
+  projectName: 'KnifeHub', // Usually your repo name.
+
+  trailingSlash: false, // https://docusaurus.io/zh-CN/docs/deployment#deploying-to-github-pages
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
