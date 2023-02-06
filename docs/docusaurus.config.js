2c1e4f170201cb8e60d8dbd5a6f93f5ecf9d3534
yiyun <yiyungent@gmail.com>
2023-02-06 19:30:22 +0800

    docs(docs/docusaurus.config.js): baseUrl: '/KnifeHub'

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://moeci.com/KnifeHub',
+  url: 'https://moeci.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/KnifeHub',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
