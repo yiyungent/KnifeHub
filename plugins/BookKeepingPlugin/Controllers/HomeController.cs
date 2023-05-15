2f308c6dbb56de20eca374a166f4bcd3f25ae06f
yiyun <yiyungent@gmail.com>
2023-05-15 16:24:51 +0800

    feat(plugins/bookkeepingplugin/): add

---
+++ plugins/BookKeepingPlugin/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PluginCore;
+
+namespace ITodaysPlus.Controllers
+{
+    /// <summary>
+    /// 其实也可以不写这个, 直接访问 Plugins/ITodaysPlus/index.html
+    /// 
+    /// 下面的方法, 是去掉 index.html
+    /// 
+    /// 若 wwwroot 下有其它需要访问的文件, 如何 css, js, 而你又不想每次新增 action 指定返回, 则 Route 必须 Plugins/{PluginId},
+    /// 这样访问 Plugins/ITodaysPlus/css/main.css 就会访问到你插件下的 wwwroot/css/main.css
+    /// </summary>
+    [Route($"Plugins/{(nameof(ITodaysPlus))}")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            string indexFilePath = System.IO.Path.Combine(PluginPathProvider.PluginWwwRootDir(nameof(ITodaysPlus)), "index.html");
+
+            return PhysicalFile(indexFilePath, "text/html");
+        }
+
+    }
+}
