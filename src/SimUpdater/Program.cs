8b8c5835786a42c37bb354faf756c7aa4bef939b
yiyun <yiyungent@gmail.com>
2023-05-19 12:47:34 +0800

    feat(knifehub.sln,src/simupdater/): simUpdater:init

---
+++ src/SimUpdater/Program.cs
@@ -0,0 +1,10 @@
+using SimUpdater;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
