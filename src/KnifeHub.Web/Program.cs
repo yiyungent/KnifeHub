f8d9f7c35bfebd2587ed6a7a44d35c6734f812d9
yiyun <yiyungent@gmail.com>
2023-05-25 19:36:33 +0800

    feat(src/knifehub.web/program.cs): knifeHub.Web.exe 启动参数添加 --no-console 时隐藏控制台窗口
    可创建快捷方式时添加启动参数 --no-console 再放到开机启动文件夹实现开机后台运行

--- src/KnifeHub.Web/Program.cs
+++ src/KnifeHub.Web/Program.cs
@@ -3,123 +3,161 @@ using PluginCore.AspNetCore.lmplements;
 using PluginCore.Interfaces;
 using PluginCore.lmplements;
 using KnifeHub.Web.Config;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// 配置注入
-ConfigOptions configOptions = builder.Configuration.GetSection(ConfigOptions.Config).Get<ConfigOptions>();
-builder.Services.Configure<ConfigOptions>(builder.Configuration.GetSection(ConfigOptions.Config));
-
-builder.WebHost.UseSentry(o =>
+namespace KnifeHub.Web
 {
-    o.Dsn = "https://ed1fa11da1bb47188350fbcfcb6af159@o4504597240741888.ingest.sentry.io/4504597253980160";
-    // When configuring for the first time, to see what the SDK is doing:
-    //o.Debug = true;
-    // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-    // We recommend adjusting this value in production.
-    o.TracesSampleRate = 1.0;
-});
-
-//Sentry.SentrySdk.CaptureMessage("Hello Sentry");
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    // 防止相同的 SchemaId 冲突
-    // https://stackoverflow.com/questions/61881770/invalidoperationexception-cant-use-schemaid-the-same-schemaid-is-already-us
-    options.CustomSchemaIds(type => type.ToString());
-});
-
-#region 跨域配置
-if (configOptions.AllowAllCors)
-{
-    Console.WriteLine("跨域: AllowAllCors");
-    builder.Services.AddCors(m => m.AddPolicy("AllowAllCors", a => a.AllowAnyOrigin().AllowAnyHeader()));
-}
-else
-{
-    Console.WriteLine("跨域: AllowedCorsOrigins");
-    #region CorsWhiteList
-    var corsWhiteList = configOptions.CorsWhiteList;
-    // 所有允许跨域的 Origin
-    List<string> allAllowedCorsOrigins = new List<string>();
-    if (corsWhiteList != null && corsWhiteList.Count > 0)
+    public class Program
     {
-        foreach (var item in corsWhiteList)
-        {
-            allAllowedCorsOrigins.Add(item);
-        }
+        #region Fields
+        // 导入 WinApi
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
 
-        // 允许 AspNetCoreClient 跨域请求
-        builder.Services.AddCors(options =>
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hwnd, int cmdShow);
+
+        // 定义可见性常量
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+        #endregion
+
+        public static void Main(string[] args)
         {
-            options.AddPolicy(name: "AllowedCorsOrigins",
-                builder =>
+            for (var i = 0; i < args.Length; i++)
+            {
+                System.Console.WriteLine($"args[{i}]: {args[i]}");
+            }
+            if (args.Contains("--no-console"))
+            {
+                System.Console.WriteLine("--no-console: hidden current console window");
+                // 获取控制台窗口句柄
+                var handle = GetConsoleWindow();
+                // 隐藏控制台窗口
+                ShowWindow(handle, SW_HIDE);
+            }
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // 配置注入
+            ConfigOptions configOptions = builder.Configuration.GetSection(ConfigOptions.Config).Get<ConfigOptions>();
+            builder.Services.Configure<ConfigOptions>(builder.Configuration.GetSection(ConfigOptions.Config));
+
+            builder.WebHost.UseSentry(o =>
+            {
+                o.Dsn = "https://ed1fa11da1bb47188350fbcfcb6af159@o4504597240741888.ingest.sentry.io/4504597253980160";
+                // When configuring for the first time, to see what the SDK is doing:
+                //o.Debug = true;
+                // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production.
+                o.TracesSampleRate = 1.0;
+            });
+
+            //Sentry.SentrySdk.CaptureMessage("Hello Sentry");
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                // 防止相同的 SchemaId 冲突
+                // https://stackoverflow.com/questions/61881770/invalidoperationexception-cant-use-schemaid-the-same-schemaid-is-already-us
+                options.CustomSchemaIds(type => type.ToString());
+            });
+
+            #region 跨域配置
+            if (configOptions.AllowAllCors)
+            {
+                Console.WriteLine("跨域: AllowAllCors");
+                builder.Services.AddCors(m => m.AddPolicy("AllowAllCors", a => a.AllowAnyOrigin().AllowAnyHeader()));
+            }
+            else
+            {
+                Console.WriteLine("跨域: AllowedCorsOrigins");
+                #region CorsWhiteList
+                var corsWhiteList = configOptions.CorsWhiteList;
+                // 所有允许跨域的 Origin
+                List<string> allAllowedCorsOrigins = new List<string>();
+                if (corsWhiteList != null && corsWhiteList.Count > 0)
                 {
-                    // ConfigOptions 里配置的白名单都允许
-                    builder.WithOrigins(allAllowedCorsOrigins.ToArray())
+                    foreach (var item in corsWhiteList)
+                    {
+                        allAllowedCorsOrigins.Add(item);
+                    }
 
-                            // 解决发送json,复杂请求问题: https://blog.csdn.net/yangyiboshigou/article/details/78738228
-                            // 解决方法: Access-Control-Allow-Headers: Content-Type
-                            // 参考: https://www.cnblogs.com/jpfss/p/10102132.html
-                            .WithHeaders("Content-Type");
-                });
-        });
+                    // 允许 AspNetCoreClient 跨域请求
+                    builder.Services.AddCors(options =>
+                    {
+                        options.AddPolicy(name: "AllowedCorsOrigins",
+                            builder =>
+                            {
+                                // ConfigOptions 里配置的白名单都允许
+                                builder.WithOrigins(allAllowedCorsOrigins.ToArray())
+
+                                        // 解决发送json,复杂请求问题: https://blog.csdn.net/yangyiboshigou/article/details/78738228
+                                        // 解决方法: Access-Control-Allow-Headers: Content-Type
+                                        // 参考: https://www.cnblogs.com/jpfss/p/10102132.html
+                                        .WithHeaders("Content-Type");
+                            });
+                    });
+                }
+                #endregion
+            }
+            #endregion
+
+            builder.Services.AddPluginCore();
+
+            var app = builder.Build();
+
+            // Performance monitoring
+            // Enable automatic tracing integration.
+            // If running with .NET 5 or below, make sure to put this middleware
+            // right after `UseRouting()`.
+            app.UseSentryTracing();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            #region 跨域配置
+            if (configOptions.AllowAllCors)
+            {
+                app.UseCors("AllowAllCors");
+                Console.WriteLine("AllowAllCors");
+            }
+            else
+            {
+                app.UseCors("AllowedCorsOrigins");
+                Console.WriteLine("AllowedCorsOrigins");
+            }
+            #endregion
+
+            app.UsePluginCore();
+
+            app.UseAuthorization();
+
+            // wwwroot
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+
+            });
+
+            app.UseDefaultFiles(new DefaultFilesOptions()
+            {
+                DefaultFileNames = new[] { "index.html" }
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
     }
-    #endregion
 }
-#endregion
-
-builder.Services.AddPluginCore();
-
-var app = builder.Build();
-
-// Performance monitoring
-// Enable automatic tracing integration.
-// If running with .NET 5 or below, make sure to put this middleware
-// right after `UseRouting()`.
-app.UseSentryTracing();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-#region 跨域配置
-if (configOptions.AllowAllCors)
-{
-    app.UseCors("AllowAllCors");
-    Console.WriteLine("AllowAllCors");
-}
-else
-{
-    app.UseCors("AllowedCorsOrigins");
-    Console.WriteLine("AllowedCorsOrigins");
-}
-#endregion
-
-app.UsePluginCore();
-
-app.UseAuthorization();
-
-// wwwroot
-app.UseStaticFiles(new StaticFileOptions()
-{
-
-});
-
-app.UseDefaultFiles(new DefaultFilesOptions()
-{
-    DefaultFileNames = new[] { "index.html" }
-});
-
-app.MapControllers();
-
-app.Run();
