d692b8c28b25124e4d28ee6aa25cfd3e32d8883b
yiyun <yiyungent@gmail.com>
2023-06-22 15:30:56 +0800

    feat(deploy/): zeabur

---
+++ deploy/zeabur/zeabur-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# region env
+export ASPNETCORE_URLS="http://+:$PORT"
+export ASPNETCORE_ENVIRONMENT="Production"
+export TZ="Asia/Shanghai"
+# endregion env
+
+# region PluginCore
+echo ${PLUGINCORE_ADMIN_USERNAME}
+echo ${PLUGINCORE_ADMIN_PASSWORD}
+
+mkdir App_Data
+
+touch /app/App_Data/PluginCore.Config.json
+
+cat '/app/zeabur-PluginCore.Config.json' | sed "s/PLUGINCORE_ADMIN_USERNAME/${PLUGINCORE_ADMIN_USERNAME}/g" | tee '/app/App_Data/PluginCore.Config.json'
+cat '/app/App_Data/PluginCore.Config.json' | sed "s/PLUGINCORE_ADMIN_PASSWORD/${PLUGINCORE_ADMIN_PASSWORD}/g" | tee '/app/App_Data/PluginCore.Config.json'
+# endregion PluginCore
+
+# cat '/etc/nginx/sites-enabled/default' | sed "s/80/${PORT}/g" | tee '/etc/nginx/sites-enabled/default'
+# /usr/sbin/nginx -s reload
+
+dotnet KnifeHub.Web.dll
+# /usr/bin/supervisord -c /etc/supervisord.conf
