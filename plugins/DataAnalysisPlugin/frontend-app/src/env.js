d46d780876fe7465b7fc2af752b4e03d413bf43a
yiyun <yiyungent@gmail.com>
2023-12-14 15:55:08 +0800

    feat(plugins/dataanalysisplugin): deleted

--- plugins/DataAnalysisPlugin/frontend-app/src/env.js
+++
@@ -1,6 +0,0 @@
-// import packageJson from '../package.json' // 报错
-const { version } = require("../package.json");
-
-module.exports = {
-  version: version, // process.env.npm_package_version,
-};
