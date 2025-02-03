749f1bf5f7d5c800b5320bbd327cabe785b29e2e
yiyun <yiyungent@gmail.com>
2025-02-03 17:57:36 +0800

    fix(src/knifehub.web/utils/dotnetutil.cs): system.ArgumentException: The path is empty
    at KnifeHub.Web.Utils.DotNetUtil.get_AspNetCoreFileVersion()
    
    fix #32

--- src/KnifeHub.Web/Utils/DotNetUtil.cs
+++ src/KnifeHub.Web/Utils/DotNetUtil.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Reflection;
+
 namespace KnifeHub.Web.Utils
 {
     public class DotNetUtil
@@ -21,9 +23,30 @@ namespace KnifeHub.Web.Utils
         {
             get
             {
+                string? aspNetCoreVersion = null;
                 var assembly = typeof(Microsoft.AspNetCore.Http.HttpContext).Assembly;
-                var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
-                string? aspNetCoreVersion = fileVersionInfo.FileVersion;
+                if (string.IsNullOrEmpty(assembly.Location))
+                {
+                    // 当程序集是动态加载的或者是从内存中加载的（比如在ASP.NET Core应用中，某些程序集可能被嵌入到主程序集中，或者通过AssemblyLoadContext动态加载），此时Location属性可能为空。
+                    // 使用 AssemblyFileVersionAttribute 获取文件版本
+                    try
+                    {
+                        var fileVersionAttr = assembly.GetCustomAttribute<System.Reflection.AssemblyFileVersionAttribute>();
+                        aspNetCoreVersion = fileVersionAttr?.Version;
+                    }
+                    catch (Exception ex)
+                    { }
+                }
+                else
+                {
+                    try
+                    {
+                        var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+                        aspNetCoreVersion = fileVersionInfo.FileVersion;
+                    }
+                    catch (Exception ex)
+                    { }
+                }
 
                 return aspNetCoreVersion;
             }
@@ -33,9 +56,29 @@ namespace KnifeHub.Web.Utils
         {
             get
             {
+                string? aspNetCoreVersion = null;
                 var assembly = typeof(Microsoft.AspNetCore.Http.HttpContext).Assembly;
-                var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
-                string? aspNetCoreVersion = fileVersionInfo.ProductVersion;
+                if (string.IsNullOrEmpty(assembly.Location))
+                {
+                    try
+                    {
+                        var versionAttr1 = assembly.GetCustomAttribute<System.Reflection.AssemblyVersionAttribute>();
+                        var versionAttr2 = assembly.GetCustomAttribute<System.Reflection.AssemblyInformationalVersionAttribute>();
+                        aspNetCoreVersion = versionAttr1?.Version ?? "null" + "+" + versionAttr2?.InformationalVersion ?? "null";
+                    }
+                    catch (Exception ex)
+                    { }
+                }
+                else
+                {
+                    try
+                    {
+                        var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+                        aspNetCoreVersion = fileVersionInfo.ProductVersion;
+                    }
+                    catch (Exception ex)
+                    { }
+                }
 
                 return aspNetCoreVersion;
             }
